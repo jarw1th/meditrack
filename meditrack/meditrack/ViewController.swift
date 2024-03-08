5ce8c2e1dc3296b44863bda1677f24f1a33f451b
jarw1th <parastaev31@gmail.com>
2024-03-08 21:23:12 +0300

    initial

---
+++ meditrack/meditrack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  meditrack
+//
+//  Created by Руслан Парастаев on 08.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+
+
+}
+
